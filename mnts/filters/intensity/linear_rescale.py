8972d2c7e8d1962d6f9a8aab8ae0cbdbfe532b86
alabamagan <fromosia@gmail.com>
2021-09-15 19:47:06 +0800

    Add option to keep standard deviation in LinearRescale

--- mnts/filters/intensity/linear_rescale.py
+++ mnts/filters/intensity/linear_rescale.py
@@ -1,7 +1,7 @@
 import SimpleITK as sitk
 import numpy as np
 from pathlib import Path
-from typing import Union, Tuple, List
+from typing import Union, Tuple, List, Optional
 from ..mnts_filters import MNTSFilter
 from .intensity_base import MNTSIntensityBase
 
@@ -14,16 +14,18 @@ class LinearRescale(MNTSIntensityBase, MNTSFilter):
         .. math::
             z = \left( \frac{x - \mu}{\sigma}\right) \times \sigma'+\mu'
 
+        The default setting is identical to Zscore normalization.
+
     Attributes:
-        mean (float):
-            Target mean value.
+        mean (float, Optional):
+            Target mean value. Default to 0.
         std (float):
-            Target std value.
+            Target std value. Default to 1. If a value <= 0 is given, the std remains the same.
 
     """
     def __init__(self,
-                 mean: float = None,
-                 std: float = None):
+                 mean: Optional[float] = 0,
+                 std: Optional[float] = 0):
         super(LinearRescale, self).__init__()
         self._mean = mean
         self._std = std
@@ -62,10 +64,12 @@ class LinearRescale(MNTSIntensityBase, MNTSFilter):
             input_std = f.GetVariance() ** .5
 
         # !!!TODO: Overflow protection
+        out_std = input_std if self._std <= 0 else self._std
+
         # Cast to float if not float already.
         if input.GetPixelID() not in [8, 9]:
             self._logger.warning("Casting the")
             input = sitk.Cast(input, sitk.sitkFloat32)
-        input = ((input - input_mean) / input_std + self._mean) * self._std
+        input = ((input - input_mean) / input_std + self._mean) * out_std
         return input
 
