eb86121d8cbd2ed9d60e21783b6082e5b901261f
alabamagan <fromosia@gmail.com>
2022-02-24 17:34:05 +0800

    Update unittest for examples

--- unit_test/Test_example.py
+++ unit_test/Test_example.py
@@ -1,35 +1,43 @@
 import unittest
 import os
 import subprocess
+# import importlib
 
 class TestScript(unittest.TestCase):
     def test_example_1(self):
+        from examples.EG01_using_the_filters import main
         os.chdir("../examples")
-        subprocess.Popen("01_using_the_filters.py", cwd="../examples")
+        main()
 
     def test_example_2(self):
+        from examples.EG02_using_filter_w_pipeline import main
         os.chdir("../examples")
-        exec(open("02_using_filter_w_pipeline.py").read())
+        main()
 
     def test_example_3(self):
+        from examples.EG03_using_filter_w_graph import main
         os.chdir("../examples")
-        exec(open("03_using_filter_w_graph.py").read())
+        main()
 
     def test_example_4(self):
+        from examples.EG04_using_filters_that_require_train import main
         os.chdir("../examples")
-        exec(open("04_using_filters_that_require_train.py").read())
+        main()
 
     def test_example_5(self):
+        from examples.EG05_another_graph_example import main
         os.chdir("../examples")
-        exec(open("05_another_graph_example.py").read())
+        main()
 
     def test_example_6(self):
+        from examples.EG06_mpi_execute import main
         os.chdir("../examples")
-        exec(open("06_mpi_execute.py").read())
+        main()
 
     def test_example_7(self):
+        from examples.EG07_create_graph_from_yaml import main
         os.chdir("../examples")
-        exec(open("07_create_graph_from_yaml.py").read())
+        main()
 
 
 if __name__ == '__main__':
