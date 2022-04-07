291e2b6a4423dc521e0c11548f1b53361f9b37c7
alabamagan <fromosia@gmail.com>
2022-04-07 15:56:32 +0800

    Update DICOM to nii

--- mnts/io/data_formatting.py
+++ mnts/io/data_formatting.py
@@ -12,147 +12,187 @@ from mnts.utils.preprocessing import recursive_list_dir
 __all__ = ['dicom2nii', 'batch_dicom2nii']
 
 def dicom2nii(folder: str,
-              out_dir: str =None,
+              out_dir: str = None,
               seq_filters: list or str = None,
               idglobber: str = None,
               use_patient_id: bool = False,
               use_top_level_fname: bool = False,
               input = None,
-              idlist = None) -> None:
+              idlist = None,
+              prefix = "",
+              debug = False,
+              dump_meta_data = False) -> None:
     """
     Covert a series under specified folder into an nii.gz image.
     This tries to assign a unique ID to each of the converted images, either based on their patient id DICOM tag or
     the folder containing the image series.
     """
+
     workerid = mpi.current_process().name
     logger = Logger['utils.dicom2nii-%s'%workerid]
     logger.info(f"Handling: {folder}")
 
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir, exist_ok=True)
-        assert os.path.isdir(out_dir), "Output dir was not made."
 
-    if not os.path.isdir(folder):
-        logger.error(f"Cannot locate specified folder: {folder}")
-        raise IOError("Cannot locate specified folder!")
+    try:
+        if not os.path.isdir(out_dir):
+            logger.info(f"Trying to make folder at {str(out_dir)}")
+            os.makedirs(out_dir, exist_ok=True)
+            assert os.path.isdir(out_dir), "Output dir was not made."
 
-    # Default globber
-    if not isinstance(idglobber, str):
-        idglobber = "(?i)(NPC|P|RHO|T1rhoNPC)?[0-9]{3,5}"
+        if not os.path.isdir(folder):
+            logger.error(f"Cannot locate specified folder: {folder}")
+            raise IOError("Cannot locate specified folder!")
 
-    folder = os.path.abspath(folder)
-    logger.debug(f"{folder}")
-    f = folder.replace('\\', '/')
+        # Default globber
+        if not isinstance(idglobber, str):
+            idglobber = "(?i)(NPC|P|RHO|T1rhoNPC)?[0-9]{3,5}"
 
-    matchobj = re.search(idglobber, os.path.basename(f))
+        # Construct paths
+        folder = Path(folder)
+        folder = str(folder.resolve().absolute())
+        logger.debug(f"{folder}")
+        f = folder
+        out_dir = str(Path(out_dir).resolve().absolute())
 
-    if not matchobj is None:
-        prefix1 = matchobj.group()
-    else:
-        prefix1 = "NA"
 
-    if use_top_level_fname:
-        path = os.path.normpath(folder.replace(input, '')).lstrip(os.sep) # lstrip to make sure its not starting from /
-        logger.debug(f"{path.split(os.sep)}")
-        prefix1 = path.split(os.sep)[0]
-    logger.debug(f"ID: {prefix1}")
+        matchobj = re.search(idglobber, os.path.basename(f))
 
-    # Check ID list and drop if input is not in idlist (not None)
-    if not idlist is None:
-        if not prefix1 in idlist:
-            logger.info(f"Skipping {prefix1} because target not in idlist.")
-            return
+        if not matchobj is None:
+            prefix1 = matchobj.group()
+        else:
+            prefix1 = "NA"
 
-    # Read file
-    series = sitk.ImageSeriesReader_GetGDCMSeriesIDs(f)
+        if use_top_level_fname:
+            path = str(folder.replace(str(Path(input).absolute()), '/')).lstrip(os.sep) # lstrip to make sure its not starting from /
+            logger.debug(f"{folder.replace(str(Path(input).absolute()), '/')}")
+            logger.debug(f"{path.split(os.sep)}")
+            prefix1 = path.split(os.sep)[0]
+        logger.debug(f"ID: {prefix1}")
 
-    for ss in series:
-        logger.debug(f"{ss}")
-        reader = sitk.ImageSeriesReader()
-        reader.SetFileNames(sitk.ImageSeriesReader_GetGDCMSeriesFileNames(
-            f,
-            ss
-        ))
-        outimage = reader.Execute()
+        # Check ID list and drop if input is not in idlist (not None)
+        if not idlist is None:
+            if not prefix1 in idlist:
+                logger.info(f"Skipping {prefix1} because target not in idlist.")
+                return
 
-        # Generate out file name
-        headerreader = sitk.ImageFileReader()
-        headerreader.SetFileName(reader.GetFileNames()[0])
-        headerreader.LoadPrivateTagsOn()
-        headerreader.ReadImageInformation()
+        # Read file
+        logger.debug(f"Reading from: {f}")
+        series = sitk.ImageSeriesReader_GetGDCMSeriesIDs(f)
 
-        # Get all the tags first
-        tags = {
-            '0010|0020': None, # PID
-            '0008|103e': None, # Study description, usually they put protocol name here
-            '0020|0011': None, # Series Number
-        }
-        for dctag in tags:
-            try:
-                tags[dctag] = headerreader.GetMetaData(dctag).rstrip().rstrip(' ')
-            except RuntimeError:
-                logger.warning(f"Tag [{dctag}] missing for image {f}")
-                tags[dctag] = 'Missing'
+        for ss in series:
+            logger.debug(f"{ss}")
+            reader = sitk.ImageSeriesReader()
+            reader.SetFileNames(sitk.ImageSeriesReader_GetGDCMSeriesFileNames(
+                f,
+                ss
+            ))
+            outimage = reader.Execute()
 
-        # Warn if pid and original prefix is not the same
-        pid = tags['0010|0020'].rstrip(' ')
-        if pid != prefix1:
-            logger.warning(f"Prefix and patient ID are not the same! (folder: {prefix1}, PID: {pid})")
+            # Generate out file name
+            headerreader = sitk.ImageFileReader()
+            headerreader.SetFileName(reader.GetFileNames()[0])
+            headerreader.LoadPrivateTagsOn()
+            headerreader.ReadImageInformation()
 
-        # Replace prefix if use patient id for file id
-        if use_patient_id:
-            prefix1 = pid
+            # Get all the tags first
+            tags = {
+                '0010|0020': None, # PID
+                '0008|103e': None, # Study description, usually they put protocol name here
+                '0020|0011': None, # Series Number
+            }
+            for dctag in tags:
+                try:
+                    tags[dctag] = headerreader.GetMetaData(dctag).rstrip().rstrip(' ')
+                except RuntimeError:
+                    logger.warning(f"Tag [{dctag}] missing for image {f}")
+                    tags[dctag] = 'Missing'
 
-        outname = out_dir + '/%s-%s+%s.nii.gz'%(prefix1,
-                                                re.sub(' +', '_', tags['0008|103e']),
-                                                tags['0020|0011']) # Some series has the same series name, need this to differentiate
+            # Warn if pid and original prefix is not the same
+            pid = tags['0010|0020'].rstrip(' ')
+            if pid != prefix1:
+                logger.warning(f"Prefix and patient ID are not the same! (folder: {prefix1}, PID: {pid})")
 
-        # Skip if dicom tag (0008|103e) contains substring in seq_filters
-        if not seq_filters is None:
-            if isinstance(seq_filters, list):
-                regex = "("
-                for i, fil in enumerate(seq_filters):
-                    regex += '(.*' + fil + '{1}.*)'
-                    if i != len(seq_filters) - 1:
-                        regex += '|'
-                regex += ')'
-                if re.match(regex, tags['0008|103e']) is None:
-                    logger.warning("skipping ", tags['0008|103e'], "from ", f)
-                    continue
-            elif isinstance(seq_filters, str):
-                if re.match(seq_filters, tags['0008|103e']) is None:
-                    logger.warning("skipping ", tags['0008|103e'], "from ", f)
-                    continue
+            # Replace prefix if use patient id for file id
+            if use_patient_id:
+                logger.info(f"Replacing prefix with DICOM tag PID: {pid}")
+                prefix2 = pid
+            else:
+                prefix2 = prefix1
 
-        # Write image
-        logger.info(f"Writting: {outname}")
-        outimage.SetMetaData('intent_name', headerreader.GetMetaData('0010|0020').rstrip())
-        sitk.WriteImage(outimage, outname)
-        del reader
+            # Add prefix
+            if len(prefix) > 0:
+                prefix2 = prefix + prefix2
+
+            description = re.sub(' +', '_', tags['0008|103e'])
+            description = re.sub('\/+', '_', description)       # Remove slash because some people are stupid and put it in description
+            outname = out_dir + '/%s-%s+%s.nii.gz'%(prefix2,
+                                                    description,
+                                                    tags['0020|0011']) # Some series has the same series name, need this to differentiate
+
+            # Skip if dicom tag (0008|103e) contains substring in seq_filters
+            if not seq_filters is None:
+                if isinstance(seq_filters, list):
+                    regex = "("
+                    for i, fil in enumerate(seq_filters):
+                        regex += '(.*' + fil + '{1}.*)'
+                        if i != len(seq_filters) - 1:
+                            regex += '|'
+                    regex += ')'
+                    if re.match(regex, tags['0008|103e']) is None:
+                        logger.warning("skipping ", tags['0008|103e'], "from ", f)
+                        continue
+                elif isinstance(seq_filters, str):
+                    if re.match(seq_filters, tags['0008|103e']) is None:
+                        logger.warning("skipping ", tags['0008|103e'], "from ", f)
+                        continue
+
+            # Write image
+            logger.info(f"Writting: {outname}")
+            outimage.SetMetaData('intent_name', headerreader.GetMetaData('0010|0020').rstrip())
+            sitk.WriteImage(outimage, outname)
+
+            # Write metadata
+            if dump_meta_data:
+                meta_data_dir = outname.replace('.nii.gz', '.json')
+                all_dicom_tags = {k: headerreader.GetMetaData(k) for k in headerreader.GetMetaDataKeys()}
+                if Path(meta_data_dir).is_file():
+                    logger.warning(f"Overwriting {str(meta_data_dir)}")
+                with open(str(meta_data_dir), 'w') as jf:
+                    json.dump(all_dicom_tags, jf)
+
+            del reader
+    except Exception as e:
+        logger.exception(e)
+        return 1
 
 
 def batch_dicom2nii(folderlist, out_dir,
                     workers=8,
-                    seq_fileters=None,
-                    idglobber = None,
-                    use_patient_id = False,
-                    use_top_level_fname = False,
-                    input = None,
-                    idlist = None):
+                    **kwargs
+                    ):
     r"""
     Batch version, use entry point in script instead of using this directly.
+
+    See Also:
+        func:`dicom2nii`
     """
     import multiprocessing as mpi
     logger = Logger['mpi_dicom2nii']
 
-    pool = mpi.Pool(workers)
-    for f in folderlist:
-        logger.info(f"Creating job for: {f}.")
-        # dicom2nii(f, out_dir, seq_fileters, idglobber, use_patient_id,use_top_level_fname, input, idlist)
-        pool.apply_async(dicom2nii, args=[f, out_dir, seq_fileters, idglobber, use_patient_id, use_top_level_fname, input, idlist])
-    pool.close()
-    pool.join()
-
-
+    if workers > 1:
+        pool = mpi.Pool(workers)
+        for f in folderlist:
+            logger.info(f"Creating job for: {f}.")
+            func = partial(dicom2nii,
+                           out_dir = out_dir,
+                           **kwargs
+                           )
+            # dicom2nii(f, out_dir, seq_fileters, idglobber, use_patient_id,use_top_level_fname, input, idlist)
+            pool.apply_async(func, args=[f])
+        pool.close()
+        pool.join()
+    else:
+        for f in folderlist:
+            dicom2nii(f, out_dir=out_dir, **kwargs)
 
 
--- mnts/scripts/dicom2nii.py
+++ mnts/scripts/dicom2nii.py
@@ -23,26 +23,38 @@ def dicom2nii(a, logger):
     logger.info(f"Dirs:\n{dicom_dirs}")
 
     data_formatting.batch_dicom2nii(dicom_dirs,
-                                    a.output,
-                                    None, # TODO: Port these two arguments to command too
-                                    None,
-                                    a.idglobber,
-                                    a.usepid,
-                                    a.usefname,
-                                    a.input,
-                                    ids)
+                                    out_dir = a.output,
+                                    workers = a.num_workers,
+                                    seq_filters = None,
+                                    idglobber = a.idglobber,
+                                    use_patient_id = a.usepid,
+                                    use_top_level_fname = a.usefname,
+                                    input = a.input,
+                                    idlist = ids,
+                                    prefix = a.prefix,
+                                    debug = a.debug,
+                                    dump_meta_data = a.dump_dicom_tags)
 
-def console_entry():
+
+def console_entry(raw_args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, action='store', dest='input',
                         help='Input directory that contains the DICOMs.')
-    parser.add_argument('-o', '--output', type=str, action='store', dest='output',
+    parser.add_argument('-o', '--output', type=str, action='store', dest='output', default= None,
                         help='Output directory to hold the nii files.')
     parser.add_argument('-d', '--depth', type=int, action='store', default=3, dest='depth',
                         help='Depth of DICOM file search.')
     parser.add_argument('-g', '--idglobber', action='store', default=None, dest='idglobber',
                         help='Specify the globber to glob the ID from the DICOM paths.')
-    parser.add_argument('--idlist', action='store', dest='idlist',
+    parser.add_argument('-n', '--num-workers', type=int, default=None,
+                        help="Specify number of workers. If not specified, use all CPU cores.")
+    parser.add_argument('--dump-dicom-tags', action='store_true',
+                        help="If this option is specified, the dicom tags will be generated to a json text file.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Debug mode.")
+    parser.add_argument('--prefix', default="", type=str,
+                        help="Add a preffix to the patient's ID.")
+    parser.add_argument('--idlist', action='store', dest='idlist', default=None, type=str,
                         help='Only do conversion if the globbed ID is in the list. e.g. ["ID1", "ID2", ...]')
     parser.add_argument('--use-top-level-fname', action='store_true', dest='usefname',
                         help='Use top level file name immediately after the input directory as ID.')
@@ -50,9 +62,9 @@ def console_entry():
                         help='Use patient id as file id.')
     parser.add_argument('--log', action='store_true', dest='log',
                         help='Keep log file under ./dicom2nii.log')
-    a = parser.parse_args()
+    a = parser.parse_args(raw_args)
 
-    logger = Logger('./dicom2nii.log', logger_name='dicom2nii', verbose=True, keep_file=a.log)
+    logger = MNTSLogger('./dicom2nii.log', logger_name='dicom2nii', verbose=True, keep_file=a.log)
     logger.info("Recieve argumetns: {}".format(a))
 
     dicom2nii(a, logger)
