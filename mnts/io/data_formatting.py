325dcd546c47b592651d8af3e917c15f294135c1
alabamagan <fromosia@gmail.com>
2023-07-19 11:57:02 +0800

    Add documents Add a method to read dicom segmentation Untested change to use dcm2nii converter class intead of as a function

--- mnts/io/data_formatting.py
+++ mnts/io/data_formatting.py
@@ -5,19 +5,73 @@ import re
 import multiprocessing as mpi
 import random
 import json
+import pprint
 sitk.ProcessObject_GlobalWarningDisplayOff()
 
 from functools import partial
 from pathlib import Path
-from ..mnts_logger import MNTSLogger
 from mnts.utils.preprocessing import recursive_list_dir
-from typing import Optional, Union, List, Tuple
-import pydicom
+from typing import Optional, Union, List, Tuple, Iterable
+from ..mnts_logger import MNTSLogger
+
+# These are optional packages installed by `pip install -e .[dicom]`
+try:
+    import pydicom
+    import pydicom_seg
+    PYDICOM_SEG_AVAILABLE = True
+except:
+    PYDICOM_SEG_AVAILABLE = False
+
 
 __all__ = ['dicom2nii', 'batch_dicom2nii']
 
 
-class DicomConverter:
+class Dcm2NiiConverter:
+    r"""A class for converting DICOM images to NIfTI format and optionally dumping metadata.
+
+    This class provides functionality to read DICOM images from a folder, convert them to NIfTI format,
+    and write them to an output directory. It also provides options to filter the images based on DICOM tags,
+    and to dump the DICOM metadata to a JSON file.
+
+    Args:
+        folder (str):
+            The folder where the DICOM images are located.
+        out_dir (str, optional):
+            The output directory where the converted NIfTI images will be saved.
+        seq_filters (list or str, optional):
+            Sequence filters for skipping DICOM series based on DICOM tag (0008|103e).
+        idglobber (str, optional):
+            Regex for extracting ID from the path.
+        use_patient_id (bool, optional):
+            Flag indicating whether to use patient ID in the file name.
+        use_top_level_fname (bool, optional):
+            Flag indicating whether to use top level file name.
+        input (str or Path):
+            Path to the input file.
+        idlist (list or tuple, optional):
+            List of IDs to process. If not None, only IDs in this list will be processed.
+        prefix (str, optional):
+            Prefix to add to the output file name.
+        debug (bool, optional):
+            Flag indicating whether to run in debug mode.
+        dump_meta_data (bool, optional):
+            Flag indicating whether to dump DICOM metadata to a JSON file.
+
+    Attributes:
+        workerid (str):
+            The ID of the current worker process.
+        logger (MNTSLogger):
+            Logger for logging information and errors.
+
+    Examples:
+    >>> from mnts.io.data_formatting import Dcm2NiiConverter
+    >>> folder = './example_folder' # DICOM images in this folder should all belong to the same scan
+    >>> out_dir = "./example_out_folder" # Where the Nii files will be placed
+    >>> converter = Dcm2NiiConverter(folder, out_dir)
+    >>> converter.Execute()
+
+
+    """
     def __init__(self,
                  folder             : str,
                  out_dir            : str                           = None,
@@ -115,10 +169,10 @@ class DicomConverter:
         # set
         return f, id_from_path
 
-    def construct_ouputpath(self, reader: sitk.ImageSeriesReader) -> Tuple[str, dict]:
+    def construct_ouputpath(self, dcm_files: Iterable[Union[str, Path]]) -> Tuple[str, dict]:
         # Generate out file name
         headerreader = sitk.ImageFileReader()
-        headerreader.SetFileName(reader.GetFileNames()[0])
+        headerreader.SetFileName(dcm_files[0])
         headerreader.LoadPrivateTagsOn()
         headerreader.ReadImageInformation()
 
@@ -168,6 +222,27 @@ class DicomConverter:
         return outname, all_dicom_tags
 
     def process_folder(self):
+        r"""Process a folder to read images, filter based on DICOM tags, write the output image, and dump metadata.
+
+        This method conducts several steps:
+        1. Pre-processes paths and checks if the ID from the path is in a predefined ID list.
+        2. Skips the folder if the ID is not in the list.
+        3. Reads image series from the folder.
+        4. For each series, it reads images, constructs the output path, and extracts DICOM tags.
+        5. If a sequence filter is set, it skips the series if the DICOM tag (0008|103e) does NOT match the filter.
+        6. Writes the read image to the constructed output path.
+        7. If the dump_meta_data flag is set, it dumps metadata to a JSON file.
+
+        This method uses SimpleITK for image reading and writing, and regex for sequence filtering.
+
+        .. warning::
+            If a file with the same name as the output JSON file already exists, it will be overwritten without warning.
+
+        Raises:
+            ImportError: If required libraries are not found.
+            ValueError: If input arguments are in wrong format or invalid.
+            FileNotFoundError: If the input folder or files do not exist.
+        """
         f, self.id_from_path = self.pre_processing_paths()
 
         # Check ID list and drop if input is not in idlist (not None). This is based on result of idglobber
@@ -218,7 +293,7 @@ class DicomConverter:
             del reader
         pass
 
-    def read_images(self, f, ss) -> Tuple[sitk.Image, sitk.ImageSeriesReader]:
+    def read_images(self, f, ss) -> Tuple[sitk.Image, List[str]]:
         r"""Read iamges from folder `f` that is identified by SID `ss`
 
         Args:
@@ -233,37 +308,56 @@ class DicomConverter:
             reader (sitk.ImageSeriesReader):
                 The reader object that is further used to read the header information.
         """
-        reader = sitk.ImageSeriesReader()
-        reader.SetFileNames(sitk.ImageSeriesReader_GetGDCMSeriesFileNames(
+        dcm_files = sitk.ImageSeriesReader_GetGDCMSeriesFileNames(
             f,
             ss
-        ))
-        outimage = reader.Execute()
-        return outimage, reader
+        )
 
-    def read_segmentation(self, f, ss):
-        """Read a DICOM file with embedded segmentation.
+        # Check if the files are segmentation
+        if PYDICOM_SEG_AVAILABLE:
+            ds = pydicom.dcmread(dcm_files[0])
+            # Check if the file contains segmentation data
+            if ds.SOPClassUID == '1.2.840.10008.5.1.4.1.1.66.4':
+                self.logger.info(f"Detect segmentation files: {pprint.pformat(dcm_files)}")
+                outimage = self.read_segmentation(dcm_files)
+                return outimage, dcm_files
+
+        # read image
+        reader = sitk.ImageSeriesReader()
+        reader.SetFileNames(dcm_files)
+        outimage = reader.Execute()
+        return outimage, dcm_files
+
+    def read_segmentation(self, filename: List[Union[str, Path]]):
+        """Read a DICOM file with embedded segmentation. This function relies on the package `pydicom` and `pydicom-seg`
+         so please make sure they are properly installed before calling.
 
         Args:
-            filename (str):
-                The path to the DICOM file.
+            filename (list):
+                The path to the DICOM file. This should be a list with a single element
 
         Returns:
             numpy.array: The segmentation as a numpy array.
         """
         # Read the DICOM file
-        ds = pydicom.dcmread(filename)
+        if not PYDICOM_SEG_AVAILABLE:
+            msg = "Segmentation is detected but loading them require `pydicom` and `pydicom-seg` packages " \
+                  "that is not installed. Reinstall this package by `pip install --force-reinstall mri-nor" \
+                  "malization-tools[dicom]`"
+            self.logger.warning(msg)
+            return
 
-        # Check if the file contains segmentation data
-        if ds.SOPClassUID != '1.2.840.10008.5.1.4.1.1.66.4':
-            raise ValueError("The file does not contain segmentation data.")
+        if not len(filename) == 1:
+            msg = "There is more than one file in the segmentation series."
+            raise ValueError(msg)
+
+        ds = pydicom.dcmread(filename[0])
+        reader = pydicom_seg.SegmentReader()
 
         # Get the segmentation from the pixel data
-        segmentation = ds.pixel_array
+        seg = reader.read(ds)
 
-        # If the DICOM file has a palette color lookup table, apply it to the segmentation
-        if 'PixelPresentation' in ds and ds.PixelPresentation == 'COLOR':
-            segmentation = apply_color_lut(segmentation, ds)
+        self.logger.info(f"Segmentation found: {seg.available_segments}")
 
         return segmentation
 
@@ -288,134 +382,19 @@ def dicom2nii(folder: str,
     workerid = mpi.current_process().name
     logger = MNTSLogger['utils.dicom2nii-%s'%workerid]
     logger.info(f"Handling: {folder}")
-
-
     try:
-        if not os.path.isdir(out_dir):
-            logger.info(f"Trying to make folder at {str(out_dir)}")
-            os.makedirs(out_dir, exist_ok=True)
-            assert os.path.isdir(out_dir), "Output dir was not made."
-
-        if not os.path.isdir(folder):
-            logger.error(f"Cannot locate specified folder: {folder}")
-            raise IOError("Cannot locate specified folder!")
-
-        # Default globber
-        if not isinstance(idglobber, str):
-            idglobber = "(?i)(NPC|P|RHO|T1rhoNPC)?[0-9]{3,5}"
-
-        # Construct paths
-        folder = Path(folder)
-        folder = str(folder.resolve().absolute())
-        logger.debug(f"{folder}")
-        f = folder
-        out_dir = str(Path(out_dir).resolve().absolute())
-        matchobj = re.search(idglobber, os.path.basename(f))
-
-        if not matchobj is None:
-            prefix1 = matchobj.group()
-        else:
-            prefix1 = "NA"
-
-        if use_top_level_fname:
-            path = str(folder.replace(str(Path(input).absolute()), '/')).lstrip(os.sep) # lstrip to make sure its not starting from /
-            logger.debug(f"{folder.replace(str(Path(input).absolute()), '/')}")
-            logger.debug(f"{path.split(os.sep)}")
-            prefix1 = path.split(os.sep)[0]
-        logger.debug(f"ID: {prefix1}")
-
-        # Check ID list and drop if input is not in idlist (not None)
-        if not idlist is None:
-            if not prefix1 in idlist:
-                logger.info(f"Skipping {prefix1} because target not in idlist.")
-                return
-
-        # Read file
-        logger.debug(f"Reading from: {f}")
-        series = sitk.ImageSeriesReader_GetGDCMSeriesIDs(f)
-
-        for ss in series:
-            logger.debug(f"{ss}")
-            reader = sitk.ImageSeriesReader()
-            reader.SetFileNames(sitk.ImageSeriesReader_GetGDCMSeriesFileNames(
-                f,
-                ss
-            ))
-            outimage = reader.Execute()
-
-            # Generate out file name
-            headerreader = sitk.ImageFileReader()
-            headerreader.SetFileName(reader.GetFileNames()[0])
-            headerreader.LoadPrivateTagsOn()
-            headerreader.ReadImageInformation()
-
-            # Get all the tags first
-            tags = {
-                '0010|0020': None, # PID
-                '0008|103e': None, # Study description, usually they put protocol name here
-                '0020|0011': None, # Series Number
-            }
-            for dctag in tags:
-                try:
-                    tags[dctag] = headerreader.GetMetaData(dctag).rstrip().rstrip(' ')
-                except RuntimeError:
-                    logger.warning(f"Tag [{dctag}] missing for image {f}")
-                    tags[dctag] = 'Missing'
-
-            # Warn if pid and original prefix is not the same
-            pid = tags['0010|0020'].rstrip(' ')
-            if pid != prefix1:
-                logger.warning(f"Prefix and patient ID are not the same! (folder: {prefix1}, PID: {pid})")
-
-            # Replace prefix if use patient id for file id
-            if use_patient_id:
-                logger.info(f"Replacing prefix with DICOM tag PID: {pid}")
-                prefix2 = pid
-            else:
-                prefix2 = prefix1
-
-            # Add prefix
-            if len(prefix) > 0:
-                prefix2 = prefix + prefix2
-
-            description = re.sub(' +', '_', tags['0008|103e'])
-            description = re.sub('\/+', '_', description)       # Remove slash because some people are stupid and put it in description
-            outname = out_dir + '/%s-%s+%s.nii.gz'%(prefix2,
-                                                    description,
-                                                    tags['0020|0011']) # Some series has the same series name, need this to differentiate
-
-            # Skip if dicom tag (0008|103e) contains substring in seq_filters
-            if not seq_filters is None:
-                if isinstance(seq_filters, list):
-                    regex = "("
-                    for i, fil in enumerate(seq_filters):
-                        regex += '(.*' + fil + '{1}.*)'
-                        if i != len(seq_filters) - 1:
-                            regex += '|'
-                    regex += ')'
-                    if re.match(regex, tags['0008|103e']) is None:
-                        logger.warning("skipping ", tags['0008|103e'], "from ", f)
-                        continue
-                elif isinstance(seq_filters, str):
-                    if re.match(seq_filters, tags['0008|103e']) is None:
-                        logger.warning("skipping ", tags['0008|103e'], "from ", f)
-                        continue
-
-            # Write image
-            logger.info(f"Writting: {outname}")
-            outimage.SetMetaData('intent_name', headerreader.GetMetaData('0010|0020').rstrip())
-            sitk.WriteImage(outimage, outname)
-
-            # Write metadata
-            if dump_meta_data:
-                meta_data_dir = outname.replace('.nii.gz', '.json')
-                all_dicom_tags = {k: headerreader.GetMetaData(k) for k in headerreader.GetMetaDataKeys()}
-                if Path(meta_data_dir).is_file():
-                    logger.warning(f"Overwriting {str(meta_data_dir)}")
-                with open(str(meta_data_dir), 'w') as jf:
-                    json.dump(all_dicom_tags, jf)
-
-            del reader
+        converter = Dcm2NiiConverter(folder,
+                                     out_dir,
+                                     seq_filters,
+                                     idglobber,
+                                     use_patient_id,
+                                     use_top_level_fname,
+                                     input,
+                                     idlist,
+                                     prefix,
+                                     debug,
+                                     dump_meta_data)
+        converter.Execute()
     except Exception as e:
         logger.exception(e)
         return 1
