a11e682ba530caf8290c219882f7fe6b3228054d
alabamagan <fromosia@gmail.com>
2023-07-20 20:31:45 +0800

    Supports setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
 # pip install -e .
-
-from setuptools import setup, find_packages
-from setuptools.extension import Extension
-import os
-
-setup(
-    name='mri-normalization-tools',
-    version='0.2.0',
-    packages=find_packages(),
-    url='',
-    license='MIT',
-    author='ML, Wong',
-    author_email="nil",
-    description='',
-    install_requires=[r.strip() for r in open('requirements.txt').readlines()],
-    entry_points = {
-        'console_scripts': [
-            'mnts-train=mnts.scripts.normalization:run_graph_train',
-            'mnts-infer=mnts.scripts.normalization:run_graph_inference',
-            'mnts-dicom2nii=mnts.scripts.dicom2nii:console_entry'
-        ]
-    }
-)
+from setuptools import setup
+setup()
