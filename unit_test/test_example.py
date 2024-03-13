6aa41d4140dce52323587def64ac8acde220210a
lwong <fromosia@gmail.com>
2024-03-13 15:59:03 +0800

    Update test for examples

--- unit_test/test_example.py
+++ unit_test/test_example.py
@@ -1,42 +1,44 @@
 import unittest
 import os
 import subprocess
+import sys
+sys.path.append(os.path.abspath("../examples"))
 # import importlib
 
 class TestScript(unittest.TestCase):
     def test_example_1(self):
-        from examples.EG01_using_the_filters import main
         os.chdir("../examples")
+        from EG01_using_the_filters import main
         main()
 
     def test_example_2(self):
-        from examples.EG02_using_filter_w_pipeline import main
         os.chdir("../examples")
+        from EG02_using_filter_w_pipeline import main
         main()
 
     def test_example_3(self):
-        from examples.EG03_using_filter_w_graph import main
         os.chdir("../examples")
+        from EG03_using_filter_w_graph import main
         main()
 
     def test_example_4(self):
-        from examples.EG04_using_filters_that_require_train import main
         os.chdir("../examples")
+        from EG04_using_filters_that_require_train import main
         main()
 
     def test_example_5(self):
-        from examples.EG05_another_graph_example import main
         os.chdir("../examples")
+        from EG05_another_graph_example import main
         main()
 
     def test_example_6(self):
-        from examples.EG06_mpi_execute import main
         os.chdir("../examples")
+        from EG06_mpi_execute import main
         main()
 
     def test_example_7(self):
-        from examples.EG07_create_graph_from_yaml import main
         os.chdir("../examples")
+        from EG07_create_graph_from_yaml import main
         main()
 
 
